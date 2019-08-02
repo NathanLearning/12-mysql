7edd77ae7a53fbb2f6c5f895d2ac7ea6de597041
Nathan Visti <nathanvisti@gmail.com>
2019-08-02 09:26:17 -0400

    fixed the js added screencaps

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -10,9 +10,74 @@ const connection = mysql.createConnection ({
     database: 'bamazon'
 })
 
-connection.query('SELECT * FROM products', (err,res) => {
-    if(err) {
-        throw err
-    }
-    console.log(res)
-  })
+function minusInventory () {
+    inquirer.prompt ([
+        {
+            name: 'id',
+            type: 'input',
+            message: 'Enter product ID'
+        },
+        {
+            name: 'quanity',
+            type: 'input',
+            message: 'How many items would you like to order?'
+        },
+    ])
+    .then(function (input) {
+        connection.query('SELECT * FROM products', (err , res) => {
+            if (err) {
+                throw err
+            }
+        let item
+        for (var i = 0; i < res.length; i++) {
+            if (res[i].itemID === parseInt(input.id)) {
+                item = res[i]
+            }
+        }
+        if (item.stockQuanity > parseInt(input.quanity)) {
+            connection.query(
+              "UPDATE products SET ? WHERE ?",
+              [
+                {
+                  stockQuanity: (item.stockQuanity - parseInt(input.quanity))
+                },
+                {
+                  itemID: item.itemID
+                }
+              ],
+              function(error) {
+                if (error) {
+                    throw error
+                }
+                  console.log("Thank you for your business! Your total is " + "$" + parseInt(input.quanity) * item.price)
+                  displayInventory()
+              }
+            )
+          }
+          else {
+            console.log("We're sorry. We don't have enough in stock.")
+            // make to bring you back somewhere
+          }
+    })
+    })
+}
+
+function displayInventory () {
+    connection.query('SELECT * FROM products', (err , res) => {
+        if (err) {
+            throw err
+        }
+        for (var i = 0; i < res.length; i++) {
+            console.log("ID: " + res[i].itemID + " -- " + 
+                        "Product: " + res[i].productName + " -- " + 
+                        "Department: " + res[i].departmentName + " -- " + 
+                        "Price: " + "$" + res[i].price + " -- " +
+                        // What is Quanity anyways?
+                        "Stock: " + res[i].stockQuanity)
+            console.log('----------------------------------')
+        }
+        minusInventory()
+    })
+}
+
+displayInventory()
